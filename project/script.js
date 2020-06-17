d399c464926f6e8ef2e86ebe7267388009ac82db
Laimis <62464661+Laimik@users.noreply.github.com>
2020-06-17 14:55:49 +0300

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -1,30 +1,104 @@
-
-// ? лучше использовать определение переменной let, так как значения массива goods могут менятся, подтягиваясь из БД ?
-
-let goods = [
-    { title: 'Компьютер', price: 10000 },
-    { title: 'Мышь', price: 500 },
-    { title: 'Клавиатура', price: 1000 },
-    { title: 'Монитор', price: 7000 },
-];
-
-// ? Нужно использовать let
-
-let getGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h2>${title}</h2><p>${price}</p></div>`;
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
 }
 
-// Один входной параметр, убрали скобки, const из - за перезаписи, стоит поменять на let
-// Добавил параметры по умолчанию, для функции
-// Запись затрудняюсь дальше упростить
-
-let renderGoodsList = (list = [{title: 'Компьютер', price: 10000}]) => {
-    let goodsList = list.map(listItem => getGoodsItem(listItem.title, listItem.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join("");
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: 'Arab shisha', price: 5000 },
+      { title: 'Modern shisha', price: 7500 },
+      { title: 'Classic shisha', price: 9000 },
+      { title: 'Sheikh shisha', price: 30000 },
+    ];
+  }
+    
+  
+    
+  render() {
+    let listHtml = '';
+    let sum = 0;
+    
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+        sum += good.price; // Подсчет суммы в цикле        
+    });      
+      console.log(sum); // Вывод суммы      
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+    
+// Функция суммы отдельно    
+    sumPrice() {
+          let sum1 = 0;
+        this.goods.forEach(good => {
+            sum1 += good.price;            
+        });
+        console.log(sum1);
+    } 
+    
 }
 
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.sumPrice(); // Вызов отдельной функции суммы
 
 
-renderGoodsList(goods);
+
+
+class backetItem {
+    constructor(title, price){
+        this.title = title;
+        this.price = price;
+        this.quantity =1;        
+    }
+    
+}
+
+class basket {
+  constructor() {
+      this.basketgoods = [];      
+  }
+    
+    checkGoodsInBasket() {} // Проверяет наличие товара в корзине
+    
+    renderSum() {} // Пересчитывает сумму
+    
+    renderTable() {} // Перерисовывает таблицу и вставляет в html
+    
+    addGoodToBasket (){
+        // Если товар присутсвует увеличиваем колличесво в таблице на 1
+        // После каждого вызова функции пересчитываем сумму и отрисовываем таблицу
+        
+/*        
+     if checkGoodsInBasket() return true {
+         quantity +=1;
+         renderSum();
+         renderTable();
+     }
+        // Если нет, то выводим новую строку в таблицу с колличеством 1 и добавляем элемент в массив basketgoods
+        // После каждого вызова функции пересчитываем сумму и отрисовываем таблицу
+         else {             
+             this.basketgoods.push(backetItem);
+             let 
+             renderSum();
+             renderTable();
+      }  
+      */
+    }    
+  
+  
+    removeFromBasket() {} // удаляет элемент из корзины и вызыввает внутри метод перерисовки таблицы и пересчета суммы.
+}
+
 
 
