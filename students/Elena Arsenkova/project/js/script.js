3ac266682b23c8e53b39ace5b487092ed58d0dc2
arsenkovae <61830366+arsenkovae@users.noreply.github.com>
2020-06-25 09:17:43 +0300

    HW3

--- students/Elena Arsenkova/project/js/script.js
+++ students/Elena Arsenkova/project/js/script.js
@@ -1,88 +1,135 @@
-/* функции генераторы
 
-const menuItem = {
-    color: 'blue',
-    name: 'Catalog',
-    makeRed: function () {
-        this.color = 'red';
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+/*function sendRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    };
+
+    xhr.open('GET', `${API}${url}`, true);
+    xhr.send();
+
+}*/
+
+class GoodsItem {
+    constructor(product_name, price) {
+        this.product_name = product_name;
+        this.price = price;
     }
-};
-
-function MenuItem (color = 'blue', name = 'Home') {
-    this.color = color;
-    this.name = name;
+    
+    render() {
+        return `<div class="item-box" data-title="${this.product_name}" data-price="${this.price}">
+            <h2>${this.product_name}</h2>
+            <p>${this.price}</p>
+            <button name="add-to-cart">Добавить в корзину</button>
+        </div>`;
+    }
 }
 
-MenuItem.prototype.makeRed = function () {
-    this.color = 'red';
-}
-
-function CatalogMenuItem (color, name) {
-    MenuItem.call(this, color, name);
-}
-
-CatalogMenuItem.prototype = Object.create(MenuItem.prototype);
-
-CatalogMenuItem.prototype.constructor = CatalogMenuItem;
-
-*/
-
-//  ES2015 Классы
-
-const goods = [
-    { title: 'Компьютер', price: 10000 },
-    { title: 'Мышь', price: 500 },
-    { title: 'Клавиатура', price: 1000 },
-    { title: 'Монитор', price: 7000 },
-];
-
-class CatalogItem {
-    constructor(title, price) {
-       this.title = title;
-       this.price = price;
+class GoodsList {
+    constructor() {
+        this.goods = [];
         
+    }
+
+    /*fetchGoods(callback) {
+        sendRequest('/catalogData.json', (result) => {
+            this.goods = JSON.parse(result);
+            callback();
+        });
+    }*/
+
+    fetchGoods(url) {
+       return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `${API}${url}`, true);
+        xhr.onreadystatechange = () => {
+                const result = xhr.responseText;      
+                resolve(result);  
+                reject(console.log('error'));    
+            }
+        xhr.send();
+        });
+   } 
+
+
+    render() {
+        let goodsLayout = '';
+        this.goods.forEach(({ product_name, price }) => {
+            const item = new GoodsItem(product_name, price);
+            goodsLayout += item.render();
+        });
+        document.querySelector('.main').innerHTML = goodsLayout;
+    }
+}
+
+const list = new GoodsList;
+list.fetchGoods();
+
+
+class CartItem {
+    constructor(product_name, price) {
+        this.product_name = product_name;
+        this.price = price;
+        this.amount = 1;
+    }
+    
+    render() {
+        return `<div class="basket-item" data-title="${this.product_name}" data-price="${this.price}">
+            <h2>${this.product_name}</h2>
+            <p>${this.price}</p>
+            <button name="delete-from-cart" onclick="deleteCartItem()>X</button>
+        </div>`;
+        }
+
+    addMore() {
+        this.amount++;
+    }
+
+}
+
+
+class CartList {
+    constructor() {
+        this.cartGoods = [];
+        /* document.querySelector('.main').addEventListener('click', (event) =>{
+          if (event.target.name === 'add-to-cart') {
+             
+           } 
+        });*/
     }
 
     render() {
-        let htmlStr = '';
-        goods.forEach (item => {
-            htmlStr += `<div class="item-box">
-                        <img src="${item.img}" alt="">
-                        <div class="desc">
-                            <h3>${item.title}</h3>
-                            <p>${item.price} $</p>
-                            <button class="buy-btn">Buy</button>
-                        </div>
-                    </div>`
-        })
-        document.querySelector('.main').innerHTML = htmlStr;      
+        let cartLayout = '';
+        this.goods.forEach(({ product_name, price }) => {
+            const item = new CartItem(product_name, price);
+            goodsLayout += item.render();
+        });
+        document.querySelector('.basket-item').innerHTML = cartLayout;
     }
 
-    goodsPriceSummery(price){
-        let priceList = price.map(priceItem => getGoodsItem(listItem.price));
-        console.log(Object.values(priceList).reduce((a, b) => a + b, 0)); 
-    }
 
+    addItemToCart(product) {
+        let cartItem = this.goods.filter(element => element.title == product.title)[0]
+
+            if (cartItem != undefined) {
+            cartItem.addMore();
+             } 
+            else { 
+            let item = new CartItem(product);
+            this.goods.push(item);
+             }
+        }
+
+    deleteCartItem(index) {
+        this.goods.splice(index, 1);
+        this.render();
+    }
 }
 
-let catalog = new CatalogItem();
-catalog.render();
-catalog.goodsPriceSummery(goods);
 
-class BasketItem {
-    constructor(title, price) {
-       this.title = title;
-       this.price = price;
-        
-    }
-
-    render() {  
-    }
-
-    amountItems() {  
-    }
-
-}
-
-let basket = new BasketItem('ass', 100);
-basket.render();
