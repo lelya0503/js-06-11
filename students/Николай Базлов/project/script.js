a001fe6a67aa69d10e980ae9f662c10090c5603e
Kolya <bazlovrabota@gmail.com>
2020-06-26 15:31:22 +0300

    hw5

--- students/Николай Базлов/project/script.js
+++ students/Николай Базлов/project/script.js
@@ -1,68 +1,41 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-function makeGETRequest(url) {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.onload = function () {
-            if (xhr.readyState === 4) {
-                resolve(JSON.parse(this.response));
-            } else {
-                const error = new Error(this.statusText);
-                error.code = this.status;
-                reject(error);
-            }
-        };
-        xhr.send();
-    });
-}
-
-class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-    render() {
-        return `
-            <div class="goods_item">
-                <div class="goods_img"></div>  
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-            </div>
-        `;
-    }
-}
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    fetchGoods() {
-        makeGETRequest(`${API_URL}/catalogData.json`)
-            .then(
-                //response => console.log(typeof (response)),
-                response => this.goods = response,
-                error => console.log(`Rejected ${error}`),
-            )
-            .then( () => {
-                    return this.render();
-                }
-            )
-    }
-    summary() {
-    }
-    render() {
-        let goodsLayout = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.product_name, good.price);
-            goodsLayout += goodItem.render();
-        });
-        document.querySelector('.goods_container').innerHTML = goodsLayout;
-    }
-}
-
-const list = new GoodsList();
-list.fetchGoods();
-
-//Собственно у меня сделано 1-ое и 3-е (вроде бы) задания. Второе задание я не сделал, ибо мне показалось оно
-//очень расплывчитым, учитывая, что удалить я ничего по URL не могу, добавить тоже. По сути это были бы те же GET запросы,
-//на response которых я бы подстраивал какие-то заглушки? Не понял.
+const app = new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        cartGoods: [],
+        searchLine: '',
+        blank:'',
+        //Задание 2. Видимость корзины.
+        isVisibleCart: true,
+    },
+    mounted() {
+        this.fetchGoods();
+    },
+    methods: {
+        fetchGoods() {
+            fetch(`${API_URL}/catalogData.json`)
+                .then((result) => {
+                    return result.json();
+                })
+                .then((data) => {
+                    this.goods = data;
+                })
+                // Задание 3. Проверка для если goods пустой.
+                // .then(() => {
+                //     this.goods = [];
+                // })
+        },
+        addToCart() {
+            this.cartGoods.splice(index, 1);
+        },
+        removeFromCart(id) {
+        },
+        filterGoods() {
+            //Задание 1
+            this.blank = this.searchLine;
+            this.searchLine = '';
+        },
+    },
+});
