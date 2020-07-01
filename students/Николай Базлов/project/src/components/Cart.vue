c4d63d577739ac0d5d9af0d1e03fd42efd4b3d22
Kolya <bazlovrabota@gmail.com>
2020-07-01 17:23:33 +0300

    hw6

---
+++ students/Николай Базлов/project/src/components/Cart.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="cart-item">
+            <h3>{{ item.product_name }}</h3>
+            <p>{{ item.price }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Cart",
+  props: {
+        item: {
+            type: Object,
+            required: true
+        },
+  },
+};
+
+</script>
+
+<style>
+
+.cart {
+    position: absolute;
+    width: 300px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+    background: #fff;
+    padding: 10px 15px;
+    top: 100px;
+}
+
+.cart-item {
+    margin-bottom: 20px;
+}
+
+</style>
---
+++ students/Николай Базлов/project/src/components/Error.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="error">
+        <h3 class="error-mess">Не удаётся выполнить запрос к серверу</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Error",
+};
+
+</script>
+
+<style>
+
+.error {
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: 0.5;
+}
+.error-mess {
+    color: #ffffff;
+    text-align: center;
+    margin-top: 35%;
+}
+</style>
