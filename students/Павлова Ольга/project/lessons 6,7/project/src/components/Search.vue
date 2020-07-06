3d312d9edaffbc2b2165a240451ec6c3eab11198
lelya0503 <63012398+lelya0503@users.noreply.github.com>
2020-07-06 11:22:15 +0300

    lessons 6,7

---
+++ students/Павлова Ольга/project/lessons 6,7/project/src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search">
+    <input type="text" class="goods-search" :value="text" @input="handleInput" />
+    <button type="button" class="search-button">Искать</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  props: {
+      text: {
+          required: true,
+          type: String,
+      },
+  },
+  methods: {
+      handleInput(e) {
+          this.$emit('textChange', e.target.value);
+      },
+  },
+};
+</script>
